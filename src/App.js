4f46c340415eb6f42e864f8b2a5e7b028de32637
amogh <amoghtripathi@gmail.com>
2020-09-06 18:35:02 +0530

    pretty printing of infoBoxes and table with numeral

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import InfoBox from './InfoBox';
 import Map from "./Map";
 import Table from "./Table";
-import {sortData} from "./util";
+import {sortData, prettyPrintStat} from "./util";
 import LineGraph from "./LineGraph";
 import "leaflet/dist/leaflet.css";
 
@@ -89,19 +89,19 @@ function App() {
           {/* 3 info boxes  */}
           <InfoBox 
             title="CoronaVirus Cases"
-            cases={countryInfo.todayCases} 
-            total={countryInfo.cases}/>
+            cases={prettyPrintStat(countryInfo.todayCases)} 
+            total={prettyPrintStat(countryInfo.cases)}/>
             
           <InfoBox 
             title="Recovered" 
-            cases={countryInfo.todayRecovered} 
-            total={countryInfo.recovered}
+            cases={prettyPrintStat(countryInfo.todayRecovered)} 
+            total={prettyPrintStat(countryInfo.recovered)}
             />
 
           <InfoBox 
             title="Deaths" 
-            cases={countryInfo.todayDeaths} 
-            total={countryInfo.deaths}
+            cases={prettyPrintStat(countryInfo.todayDeaths)} 
+            total={prettyPrintStat(countryInfo.deaths)}
             />
 
         </div>
